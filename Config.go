8368ac3ec8fdae57baa09894ddd986b268ea0718
Karenli <karen_lrx@163.com>
2021-03-11 23:01:57 +0800

    Config设置

--- Config.go
+++ Config.go
@@ -8,13 +8,16 @@ const MAXMESSAGECOUNT uint32 = 100
 
 // The byte count of the serialized messages in a batch cannot
 // exceed this value.
-const ABSOLUTEMAXBYTES uint32 = 10 
+const ABSOLUTEMAXBYTES uint32 = 10 * 1024 * 1024
 
 // The byte count of the serialized messages in a batch should not
 // exceed this value.
-const PREFERREDMAXBYTES uint32 = 2 
+const PREFERREDMAXBYTES uint32 = 2 * 1024 * 1024
 
-// mainNode's address
-const MAINNODEADDRESS string = "127.0.0.1:8002"
+// the local mainNode's address
+const MAINNODEADDRESSLOCAL string = "127.0.0.1:8001"
+
+// the mainNode's address for transmit PrevBlock
+const MAINNODEADDRESSMSG string = "127.0.0.1:8002"
 
 const BATCHTIMEOUT time.Duration = 2 * time.Second
